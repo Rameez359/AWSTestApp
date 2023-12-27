118c8ed0b0de02a7b1d8588b88e9579c7ca9fe82
Rameez Ali <108946108+Rameez359@users.noreply.github.com>
2023-12-27 22:29:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("hello welcome to AWS");
 });
+app.get("/get", (req, res) => {
+  res.send("Get record from S3 bucket");
+});
 app.listen(80, () => console.log("Listining on Port 3000"));
